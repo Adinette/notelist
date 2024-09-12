73f259f863876edbc05365e9015134298f06bd86
Adinette D. <adinette@wazoom-studio.com>
2024-09-13 01:01:12 +0200

    add api and database

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "notelist": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "notelistb2bbdb77": {
       "AppClientID": "string",
